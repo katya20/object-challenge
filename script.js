c961aa25429fc52236ca55a27f805b7eb8ada2d0
katya20 <kpanchenko21@gmail.com>
2016-10-17 09:08:25 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var person = {
   name: "Katya",
-  image: "http://www.myconfinedspace.com/wp-content/uploads/tdomf/119129/n35164325048_1212609_7006.jpg",
+  imageSource: "http://www.myconfinedspace.com/wp-content/uploads/tdomf/119129/n35164325048_1212609_7006.jpg",
   height: "500px",	
   favoriteColor: "lavender",
   catchphrase: "Get pumped!"
