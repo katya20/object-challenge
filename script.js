c6f244e1bbf3995387933078dcb646b417724108
katya20 <kpanchenko21@gmail.com>
2016-10-17 08:42:16 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var person = {
-  name: "David",
+  name: "Katya",
   representation: "http://i.dailymail.co.uk/i/pix/2016/03/14/19/322C3FA100000578-0-image-a-13_1457982438669.jpg",
   height: "500px",	
   favoriteColor: "blue",
